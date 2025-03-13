12d5f52a2bbdb4474495110b4e8fec692c612a7c
Cleiton Araujo <cleitonamc@gmail.com>
2025-03-12 23:29:24 -0300

    Consumer

--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -11,9 +11,9 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     //recebe o produto passado pelo privider acima (product_grid)
-    final Product product = Provider.of<Product>(context);
+    //listen false para n reconstruir o objetto inteiro quando atualizado
+    final Product product = Provider.of<Product>(context, listen: false);
 
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
@@ -32,14 +32,21 @@ class ProductItem extends StatelessWidget {
         ),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
-          leading: IconButton(
-            onPressed: () {
-              //ao mudar o estado do produto notifica os ouvintes deste produto
-              product.toggleFavorite();
-            },
-            icon: Icon(
-                product.isFavorite ? Icons.favorite : Icons.favorite_border),
-            color: Theme.of(context).colorScheme.secondary,
+
+          //um ouvinte que atualiza um ponto específico desta forma
+          // n reconstroi o widget inteiro, somente o q foi alterado
+          //atentar que não referencia diretamente o product acima
+          leading: Consumer<Product>(
+            builder: (ctx, productConsumer, _) => IconButton(
+              onPressed: () {
+                //ao mudar o estado do favorito altera o icone
+                productConsumer.toggleFavorite();
+              },
+              icon: Icon(productConsumer.isFavorite
+                  ? Icons.favorite
+                  : Icons.favorite_border),
+              color: Theme.of(context).colorScheme.secondary,
+            ),
           ),
           title: Text(
             product.title,
