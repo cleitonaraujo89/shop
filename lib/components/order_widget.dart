283e9933a8c61c5955c38226db3f48fc9bd995ff
Cleiton Araujo <cleitonamc@gmail.com>
2025-03-21 23:31:52 -0300

    Detalhes dos pedidos

--- lib/components/order_widget.dart
+++ lib/components/order_widget.dart
@@ -2,19 +2,114 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import '../providers/orders.dart';
 
-class OrderWidget extends StatelessWidget {
+class OrderWidget extends StatefulWidget {
   const OrderWidget({super.key, required this.order});
 
   final Order order;
 
   @override
+  State<OrderWidget> createState() => _OrderWidgetState();
+}
+
+class _OrderWidgetState extends State<OrderWidget> {
+  bool _expanded = false;
+  @override
   Widget build(BuildContext context) {
     return Card(
       margin: EdgeInsets.all(10),
-      child: ListTile(
-        title: Text('R\$ ${order.total}'),
-        subtitle: Text(DateFormat('dd / MM / yyyy - hh:mm').format(order.date)),
-        trailing: IconButton(onPressed: () {}, icon: Icon(Icons.expand_more)),
+      child: Column(
+        children: [
+          ListTile(
+            title: Text(
+                _expanded ? 'Detalhes do Pedido' : 'R\$ ${widget.order.total}'),
+            subtitle: Text(
+                DateFormat('dd / MM / yyyy - hh:mm').format(widget.order.date)),
+            trailing: IconButton(
+              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
+            ),
+          ),
+          if (_expanded) ...[
+            Column(
+              children: [
+                Container(
+                  height: (widget.order.products.length * 35),
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 15,
+                    vertical: 4,
+                  ),
+                  child: ListView(
+                    children: widget.order.products.map((product) {
+                      return Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            product.title,
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            '${product.quantity}x R\$ ${product.price}',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.grey,
+                            ),
+                          )
+                        ],
+                      );
+                    }).toList(),
+                  ),
+                ),
+                Divider(
+                  thickness: 2,
+                  indent: 25,
+                  endIndent: 25,
+                  color: Colors.black,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Total',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            'R\$ ',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            widget.order.total.toString(),
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.purple,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ],
+        ],
       ),
     );
   }
