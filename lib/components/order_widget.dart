1fce8e6f0353b601169e2bc129b51036a810e6f6
Cleiton Araujo <cleitonamc@gmail.com>
2025-04-11 19:49:10 -0300

    AnimatedSize nos pedidos

--- lib/components/order_widget.dart
+++ lib/components/order_widget.dart
@@ -15,101 +15,106 @@ class _OrderWidgetState extends State<OrderWidget> {
   bool _expanded = false;
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.all(10),
-      child: Column(
-        children: [
-          ListTile(
-            title: Text(
-                _expanded ? 'Detalhes do Pedido' : 'R\$ ${widget.order.total}'),
-            subtitle: Text(
-                DateFormat('dd / MM / yyyy - hh:mm').format(widget.order.date)),
-            trailing: IconButton(
-              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
-              onPressed: () {
-                setState(() {
-                  _expanded = !_expanded;
-                });
-              },
+    return AnimatedSize(
+      duration: Duration(milliseconds: 300),
+      child: Card(
+        margin: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            ListTile(
+              title: Text(_expanded
+                  ? 'Detalhes do Pedido'
+                  : 'R\$ ${widget.order.total}'),
+              subtitle: Text(DateFormat('dd / MM / yyyy - hh:mm')
+                  .format(widget.order.date)),
+              trailing: IconButton(
+                icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+                onPressed: () {
+                  setState(() {
+                    _expanded = !_expanded;
+                  });
+                },
+              ),
             ),
-          ),
-          if (_expanded) ...[
-            Column(
-              children: [
-                Container(
-                  height: (widget.order.products.length * 35),
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 15,
-                    vertical: 4,
-                  ),
-                  child: ListView(
-                    children: widget.order.products.map((product) {
-                      return Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            product.title,
-                            style: const TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
+            if (_expanded) ...[
+              Column(
+                children: [
+                  Container(
+                    height: (widget.order.products.length * 35),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 15,
+                      vertical: 4,
+                    ),
+                    child: ListView(
+                      children: widget.order.products.map((product) {
+                        return Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              product.title,
+                              style: const TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
+                            Text(
+                              '${product.quantity}x R\$ ${product.price}',
+                              style: const TextStyle(
+                                fontSize: 18,
+                                color: Colors.grey,
+                              ),
+                            )
+                          ],
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                  const Divider(
+                    thickness: 2,
+                    indent: 25,
+                    endIndent: 25,
+                    color: Colors.black,
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          'Total',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
                           ),
-                          Text(
-                            '${product.quantity}x R\$ ${product.price}',
-                            style: const TextStyle(
-                              fontSize: 18,
-                              color: Colors.grey,
-                            ),
-                          )
-                        ],
-                      );
-                    }).toList(),
-                  ),
-                ),
-                const Divider(
-                  thickness: 2,
-                  indent: 25,
-                  endIndent: 25,
-                  color: Colors.black,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text(
-                        'Total',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
                         ),
-                      ),
-                      Row(
-                        children: [
-                          const Text(
-                            'R\$ ',
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
+                        Row(
+                          children: [
+                            const Text(
+                              'R\$ ',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
-                          ),
-                          Text(
-                            widget.order.total.toString(),
-                            style: const TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.purple,
+                            Text(
+                              widget.order.total.toString(),
+                              style: const TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.purple,
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ],
           ],
-        ],
+        ),
       ),
     );
   }
